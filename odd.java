c50edf7d6212d86f8f079ce50725b9a165ed1cd7
JUVENILE-KAVIYA <47383773+JUVENILE-KAVIYA@users.noreply.github.com>
2019-03-08 12:27:36 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -7,9 +7,14 @@ public class GUVI
         int num;
 Scanner sc=new Scanner(System.in);
 num=sc.nextInt();
+        if(num>0)
+        {
         if(num % 2 == 0)
             System.out.println("even");
         else
             System.out.println("odd");
+        }
+        else
+            System.out.println("invalid");
     }
 }
